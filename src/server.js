3023ac2398191a59b2d32f932a364646fa662ba5
André Luís Bordignon <albordignon@gmail.com>
2025-09-30 22:17:13 -0300

    Nova atualização

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  console.log(`Acesse: http://localhost:${PORT}`);
+  console.log(`Acesse    -----: http://localhost:${PORT}`);
 });
