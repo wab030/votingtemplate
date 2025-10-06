bb0bbd741a0130a576bdde14be02470646366996
André Luís Bordignon <albordignon@gmail.com>
2025-10-06 10:56:51 -0300

    ....

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const PORT = 4000
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodandllllo na porta ${PORT}`);
     console.log(`Acesse: http://localhost:${PORT}`);
 });
