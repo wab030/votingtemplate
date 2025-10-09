c71d76da05b813f48866bafe35a7135d00edb3f8
Joao Gimenez <jvg020804@gmail.com>
2025-10-09 20:11:59 -0300

    Primeira tentativa

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-// Aqui você deve desenvolver toda a configuração do seu app. 
+// Aqui você deve desenvolver toda a configuração do seu app..
 
 //ATENÇÂO NÃO COLOQUE O COMANDO app.listen nesse arquivo. Ele já está no server, que é o arquivo principal da sua aplicação. 
 
