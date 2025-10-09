14157ee2bf2fa223a03a1ca00f6b8bbf27bcebc4
hdelianeto <falecom@heberdelia.com.br>
2025-10-09 19:35:30 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app'); // Importa a aplicação Express
 const PORT = 5000; // Define a porta obrigatória
 
+//teste
+
 // Inicia o servidor na porta 5000
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
