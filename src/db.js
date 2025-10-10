d092df9180b5763b9495a66cff2446ce6455c7f4
Ana Ramalho <ana.ramalho@aluno.ifsp.edu.br>
2025-10-09 22:22:33 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,8 +2,9 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'admin',
-    password: 'ifsp@1234',
+    password: 'ifsp@123',
     database: 'saude'
 });
 module.exports.connect = async () => pool;
+
 
